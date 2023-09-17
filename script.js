fbf9296f3cd265fbe686bdc973e27bfe9219d6b5
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2023-09-17 15:32:03 +0530

    Updated

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 async function restApi() {
-    try {
+  try {
       const data = await fetch("https://jsonplaceholder.typicode.com/users");
       const result = await data.json();
 
-      const container = document.createElement("div");
-      const h1 = document.createElement("h1");
-      container.setAttribute("class", "container");
-      h1.innerText = "Users List";
-      container.appendChild(h1);
-    
-  
+      const container = document.querySelector('.row');
+      // No need to create a new container element
+
       for (let i = 0; i < result.length; i++) {
-      const div = document.createElement("div");
-      div.innerHTML = `
-      <div class="row row-cols-1 row-cols-md-2 g-3 m-3 p-3 ">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-            <p class="card-text"><b>Name : ${result[i].name}</b></p>
-            <p class="card-text">Email : ${result[i].email}</p>
-            <p class="card-text">Street : ${result[i].address.street}</p>
-            <p class="card-text">City : ${result[i].address.city}</p>
-            </div>
-          </div>
-        </div>
-      </div>`;
-    
-        container.appendChild(div);
+          const div = document.createElement("div");
+          div.innerHTML = `
+              <div class="col">
+                  <div class="card">
+                      <div class="card-body">
+                          <p class="card-text"><b>Name : ${result[i].name}</b></p>
+                          <p class="card-text">Email : ${result[i].email}</p>
+                          <p class="card-text">Street : ${result[i].address.street}</p>
+                          <p class="card-text">City : ${result[i].address.city}</p>
+                      </div>
+                  </div>
+              </div>
+          `;
+          container.appendChild(div);
       }
-      document.body.append(container);
-    } catch (error) {
+  } catch (error) {
       console.log(error);
-    }
   }
-  
-  restApi();
+}
 
-
+restApi();
